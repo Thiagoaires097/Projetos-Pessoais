d2ab9e428ad73045529c64a63d91792d754dc586
Thiago Aires <thiagonunestele@gmail.com>
2026-02-22 16:38:24 -0300

    Add files via upload

---
+++ Idade.py
@@ -0,0 +1,10 @@
+nome = input (" Digite Seu nome ")
+idade = int( input (" Digite sua idade "))
+
+print (f" Olá {nome}, Você tem {idade} ") 
+
+if idade >= 18: 
+    print (f" olá { nome } Você tem {idade} anos, você é maior de idade")
+    
+else:
+    print (f" olá { nome } Você tem {idade} anos, você é menor de idade")
