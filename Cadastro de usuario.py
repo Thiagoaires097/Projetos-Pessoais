d169627450e270ae4b68b2c7da4896ab3df2a514
Thiago Aires <thiagonunestele@gmail.com>
2026-02-23 10:58:45 -0300

    Cadastro de Usuario
    Aqui Criei um cadastro de usuário simples.

---
+++ Cadastro de usuario.py
@@ -0,0 +1,24 @@
+#Criando o dicionario vazio
+user_info =  {}
+
+#Dados do usuarios para cadastro
+nome = input (' Digite seu nome ')
+
+idade = input(' Digite sua idade ')
+
+email = input(' Digite seu email ')
+
+Cpf = input(' Digite seu CPF ')
+
+suite = input (' Digite o número do quarto')
+
+#Estamos guardando as informações digitada do usuario
+user_info[' nome'] = nome
+user_info[' idade'] = idade
+user_info[' email'] = email
+user_info[' CPF'] = Cpf
+user_info[' suite'] = suite
+
+print('Cliente cadastrado com sucesso')
+
+print(user_info)
