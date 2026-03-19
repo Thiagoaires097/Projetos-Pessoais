0488783fc080c8e64cfb046c3d3bf4d1f263701b
Thiago Aires <thiagonunestele@gmail.com>
2026-03-18 21:52:23 -0300

    Sistema de par ou Ímpar
    Esse sistema detecta o numero digita e informa se ele é divisível por 2 ou não

---
+++ Par_impar.py
@@ -0,0 +1,12 @@
+print("Bem Vindo")
+
+while True:
+        
+    numero=int (input(" Digite o numero desejado "))
+
+    if numero % 2 == 0:
+        print(" Esse Numero e par ")
+
+    else:
+        print (" Esse número e Impar ")
+        
